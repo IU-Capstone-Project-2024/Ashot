ad28e6464dbbd0d036adb322a9b509e8b467f220
MattWay224 <win220404@mail.ru>
2024-07-01 22:55:00 +0300

    Update implementations for actual room libraries

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -1,51 +1,51 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-	kotlin("jvm")
-	id("org.jetbrains.compose")
+    kotlin("jvm")
+    id("org.jetbrains.compose")
 }
 
 group = "test"
 version = "1.0-SNAPSHOT"
 
 repositories {
-	mavenCentral()
-	maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-	google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
 }
 
 dependencies {
-	implementation(compose.desktop.currentOs)
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
-	implementation("org.jetbrains.androidx.navigation:navigation-compose:2.7.0-alpha07")
-	implementation("androidx.compose.material:material-icons-extended:1.6.8")
-	implementation("androidx.room:room-gradle-plugin:2.6.1")
-	implementation("androidx.room:room-compiler:2.6.1:")
-	implementation("androidx.room:room-runtime-jvm:2.7.0-alpha04")
-	implementation("androidx.sqlite:sqlite-bundled-jvm:2.5.0-alpha04")
+    implementation(compose.desktop.currentOs)
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.androidx.navigation:navigation-compose:2.7.0-alpha07")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
+    implementation("androidx.room:room-gradle-plugin:2.6.1")
+    implementation("androidx.room:room-compiler:2.6.1:")
+    implementation("androidx.room:room-runtime-jvm:2.7.0-alpha04")
+    implementation("androidx.sqlite:sqlite-bundled-jvm:2.5.0-alpha04")
 }
 
 
 
 compose.desktop {
-	application {
-		mainClass = "MainKt"
+    application {
+        mainClass = "MainKt"
 
-		nativeDistributions {
-			targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-			packageName = "Debug"
-			packageVersion = "1.0.0"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Debug"
+            packageVersion = "1.0.0"
 
-			windows {
-				iconFile.set(project.file("src/main/resources/icons/icon.ico"))
-			}
-			macOS {
-				iconFile.set(project.file("src/main/resources/icons/icon.icns"))
-			}
-			linux {
-				iconFile.set(project.file("src/main/resources/icons/icon.png"))
-			}
-		}
-	}
+            windows {
+                iconFile.set(project.file("src/main/resources/icons/icon.ico"))
+            }
+            macOS {
+                iconFile.set(project.file("src/main/resources/icons/icon.icns"))
+            }
+            linux {
+                iconFile.set(project.file("src/main/resources/icons/icon.png"))
+            }
+        }
+    }
 }
